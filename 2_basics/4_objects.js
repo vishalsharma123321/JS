1f2bcf2fd702a894599bdf73c5a62569e54298ab
Vishal Sharma <122232612+vishalsharma123321@users.noreply.github.com>
2024-07-01 15:25:11 +0000

    json

--- 2_basics/4_objects.js
+++ 2_basics/4_objects.js
@@ -26,24 +26,34 @@ const regularuser={
 // and if we have a huge nested objects we can simple arrage it like this .
 // this is how we add objects .
 const obj3 = {...tinderuser,...regularuser};
-console.log(obj3);
+// console.log(obj3);
 
 // there is one more way to add other objects to one is .
 const object1 = {1:"b" , 4:"c"  }
 const object2 = {2:"b" , 3:"c"  }
 const object3 = Object.assign({},object1,object2);
-console.log(object3);
+// console.log(object3);
 
 
 // we can get all the key and object from an object like this .
-console.log(Object.keys(tinderuser));
-console.log(Object.values(tinderuser));
-console.log(Object.entries(tinderuser));
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));
+// console.log(Object.entries(tinderuser));
 
 // TO CAHECK IF THE OBJECT CONTAINS     
-console.log(tinderuser.hasOwnProperty('islogedin'));
+// console.log(tinderuser.hasOwnProperty('islogedin'));
 
 
+const course={
+    courcename:"java script",
+    courcePrice:"999",
+    courceInstructor:"vishal sharma "
+}
 
+// we we are writing a key many time then we can do this to simplify it 
+const{courceInstructor} = course; // like this 
+console.log(courceInstructor); 
 
-
+// and if we want to make it even more smaller we can do this .
+const{courceInstructor : teacher} = course; // like this 
+console.log(teacher);
